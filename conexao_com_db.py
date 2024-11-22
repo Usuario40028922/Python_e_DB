6ebba1ac9eb579e04ed44a441c2fd998ba282674
Usuario40028922 <165251305+Usuario40028922@users.noreply.github.com>
2024-11-22 14:29:18 -0300

    Update conexao_com_db.py

--- conexao_com_db.py
+++ conexao_com_db.py
@@ -3,25 +3,44 @@
 import mysql.connector
 
 
+
 #Variáveis
 mydb = mysql.connector.connect(host="localhost", user="root")
 
 cursor = mydb.cursor()
 
 bd = None
+
+
+
+
 #Funções
 def oque_vc_quer_rappa():
-  lista = [""]
+  lista = ['1 - ']
   a = input("")
   lista[0]
+  
+  
 def create_database(nome):
   cursor.execute("CREATE DATABASE IF NOT EXISTS {nome}")
   return "USE {nome};"
+
+
+
 #Início do código:
 
 print(mydb)
 
+cursor.execute("USE aula_db;")
 
-while True:
-  oque_vc_quer_rappa()
-  
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS alunos(
+  id INT AUTO_INCREMENT NOT NULL, 
+  nome VARCHAR(30),
+  idade INT,
+  PRIMARY KEY(id)
+  );
+
+
+''')
+
