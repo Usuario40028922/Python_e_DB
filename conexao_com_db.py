a17f1734c08527359ceb1c187e720be5d7e139d3
gael- <iffgabriel410@gmail.com>
2024-11-29 13:44:06 -0300

    atualiza ae

--- conexao_com_db.py
+++ conexao_com_db.py
@@ -1,10 +1,29 @@
 import mysql.connector
 
-#variáveis
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password=''
+)
 
-bd = mysql.connector.connect(host="localhost", user="root")
+cursor = connection.cursor()
 
-cursor = mydb.cursor(dictionary = true))
+cursor.execute("CREATE DATABASE IF NOT EXISTS Receita_Federal")
+cursor.execute("use Receita_Federal")
 
-cursor.execute('''CREATE DATABASE IF NOT EXISTS vitima (
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS vitima (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        renda DECIMAL(10, 2) NOT NULL,
+        cpf VARCHAR(14) NOT NULL UNIQUE,
+        nome VARCHAR(255) NOT NULL,
+        endereco VARCHAR(255) NOT NULL,
+        telefone VARCHAR(15) NOT NULL,
+        tipo_pessoa ENUM('Física', 'Jurídica') NOT NULL
+    );
+''')
 
+cursor.execute("show tables")
+
+for tabela in cursor:
+    print(tabela)
